95584abd8222a0ce6a14f6f0aa9bfa3e0308a693
Asselabdrakhman <abdrakhmanova.assel16@gmail.com>
2026-09-19 00:03:35 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='unt100-v1';const A=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
